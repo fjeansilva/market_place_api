71c49a7b1e422d4781dc5ac736e7a74abb5d1271
Jean Silva <jeancarlos.silva@me.com>
2015-11-08 10:15:00 -0200

    Adds shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe User do
+  
   before { @user = FactoryGirl.build(:user) }
 
   subject { @user }
@@ -10,4 +11,15 @@ describe User do
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+
+end
+
+describe "when email is not present" do
+	before { @user.email = " " }
+	it { should_not be_valid }
 end
